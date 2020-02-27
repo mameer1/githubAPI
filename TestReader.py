7672fbefe33329d831be3aa92c856b24bd67fe1a
mameer1 <33731371+mameer1@users.noreply.github.com>
2020-02-26 21:30:56 -0500

    Update TestReader.py

--- TestReader.py
+++ TestReader.py
@@ -1,10 +1,15 @@
 import unittest
 
+from unittest import mock
 from GithubRepoReader import main
 
 class TestGithubReader(unittest.TestCase):
-    def test1(self):
+    
+    @mock.patch('GithubRepoReader.main')
+    def test1(self, mock_main):
+        mock_main.return_value = [['firekings', 1], ['truedraco', 2]]
         self.assertEqual(main('StormNemesisgod'), [['firekings', 1], ['truedraco', 2]], 'Repo: firekings Number of commits: 1, Repo: truedraco Number of commits: 2')
+
     def test2(self):
         self.assertEqual(main(5), 'ERROR: username must be a string')
 
